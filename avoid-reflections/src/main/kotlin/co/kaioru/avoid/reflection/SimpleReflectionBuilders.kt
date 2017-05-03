26977cf3bb3342c1639831cfdc165903aaec923c
Kaioru <keifuurawrs@gmail.com>
2017-05-03 19:24:26 +0800

    Fix unwrapped type

--- avoid-reflections/src/main/kotlin/co/kaioru/avoid/reflection/SimpleReflectionBuilders.kt
+++ avoid-reflections/src/main/kotlin/co/kaioru/avoid/reflection/SimpleReflectionBuilders.kt
@@ -3,4 +3,4 @@ package co.kaioru.avoid.reflection
 import co.kaioru.retort.impl.CommandContext
 import co.kaioru.retort.reflection.builder.impl.ReflectionGeneratorBuilder
 
-open class SimpleReflectionBuilder<I : CommandContext, O : Any> : ReflectionGeneratorBuilder<I, O>()
+open class SimpleReflectionBuilder<I : CommandContext> : ReflectionGeneratorBuilder<I, Void>()
