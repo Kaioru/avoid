11d212ee035cf5559af914a97754375ea35fbf7b
Kaioru <keifuurawrs@gmail.com>
2017-05-03 18:50:11 +0800

    Add proper void commands

--- avoid-core/src/main/kotlin/co/kaioru/avoid/SimpleCommands.kt
+++ avoid-core/src/main/kotlin/co/kaioru/avoid/SimpleCommands.kt
@@ -7,7 +7,15 @@ import co.kaioru.retort.impl.CommandRegistry
 
 abstract class SimpleContext : CommandContext()
 
-abstract class SimpleCommand<I : SimpleContext>(name: String) : Command<I, Void>(name)
+abstract class SimpleCommand<I : SimpleContext>(name: String) : Command<I, Void>(name) {
+    @Suppress("UNREACHABLE_CODE")
+    override fun execute(input: I): Void {
+        executeAfter(input)
+        return null!!
+    }
+
+    abstract fun executeAfter(input: I)
+}
 
 abstract class SimpleMiddleware<I : SimpleContext> : CommandMiddleware<I>()
 
